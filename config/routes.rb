3823a910abc4231159a76b18e395b9f51091c66f
LVQA1998 <quanganlevan@gmail.com>
2020-06-02 16:00:26 +0700

    static_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    root to: "admins#index"
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    
+    namespace :admin do 
+      root to: "admins#index"
+    end
   end
 end
