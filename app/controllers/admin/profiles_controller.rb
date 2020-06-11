a5376d890f60e61a2cc2be7892d4396009c66ee0
haiphung1 <haipmph05501@fpt.edu.vn>
2020-06-11 13:21:48 +0700

    view cv

---
+++ app/controllers/admin/profiles_controller.rb
@@ -0,0 +1,17 @@
+class Admin::ProfilesController < Admin::AdminsController
+  before_action :load_profile, only: %i(show)
+
+  def show
+    @experiences = @profile.experiences
+  end
+
+  private
+
+  def load_profile
+    @profile = Profile.find_by id: params[:id]
+    return if @profile
+
+    flash[:danger] = t "admin.profile.profile_not_found"
+    redirect_to admin_root_url
+  end
+end
