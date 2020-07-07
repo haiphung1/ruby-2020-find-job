332cd9149a403eccd6d3141225b161dabe3aaaa2
haiphung1 <haipmph05501@fpt.edu.vn>
2020-07-07 09:13:13 +0700

    add gem sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
       resources :posts, except: %i(show)
       resources :users, only: %i(show)
       resources :profiles, only: %i(show)
-      resources :user_applies, only: %i(index update)
+      resources :user_applies, only: %i(index update destroy)
       resources :companies, only: %i(index edit update)
     end
   end
